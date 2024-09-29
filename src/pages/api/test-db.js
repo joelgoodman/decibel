96f60c2fcbcc268d486af87e76f060a1ac697b8c
Joel Goodman <joel@braverymedia.co>
2024-09-29 13:30:47 -0700

    oops

--- src/pages/api/test-db.js
+++ src/pages/api/test-db.js
@@ -1,10 +1,10 @@
-// pages/api/test-db.js
-import { query } from "../../utils/db";
+// src/pages/api/test-db.js
+import { connectDB } from "../../utils/db";
 
 export default async function handler(req, res) {
 	try {
-		const result = await query("SELECT NOW()");
-		res.status(200).json({ success: true, result: result.rows });
+		await connectDB();
+		res.status(200).json({ success: true });
 	} catch (error) {
 		res.status(500).json({ success: false, error: error.message });
 	}
